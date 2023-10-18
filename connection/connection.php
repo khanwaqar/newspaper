f03d3c9476f9d35810e6d7dde17a1c9886e6468a
waqar <khan.waqar278@gmail.com>
2023-10-18 18:20:28 +0500

    code commit

---
+++ connection/connection.php
@@ -0,0 +1,13 @@
+<?php
+ $servername = "localhost";
+ $database = "newspaper";
+ $username = "root";
+ $password = "waqar";
+ // Create a connection
+ $conn = mysqli_connect($servername, $username, $password, $database);
+ // Check the connection
+ if (!$conn) {
+     die("Connection failed: " . mysqli_connect_error());
+ }
+ //echo "Connected successfully";
+?> 
